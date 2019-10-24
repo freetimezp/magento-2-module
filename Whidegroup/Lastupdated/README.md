e5e78c4327bd3490149a4193aed206c8971b122f
freetimezp <kraftukrnet@ukr.net>
2019-10-24 22:16:38 +0300

    Magento. Create new module. Try to add custom attribute.

---
+++ Whidegroup/Lastupdated/README.md
@@ -0,0 +1,2 @@
+## Lastupdated Whidegroup module for Magento 2.x platform
+My new module Magento
---
+++ Whidegroup/Lastupdated/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+	ComponentRegistrar::MODULE, 
+	'Whidegroup_Lastupdated', 
+	__DIR__
+);
