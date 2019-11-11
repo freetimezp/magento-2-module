f9b7509edcaaf96403df031f20ffb2a25cd9f8d8
freetimezp <kraftukrnet@ukr.net>
2019-11-11 21:53:25 +0200

    Magento 2. Learning.

--- Devchannel/HelloWorld/Controller/Index/Index.php
+++ Devchannel/HelloWorld/Controller/Index/Index.php
@@ -1,16 +1,16 @@
 <?php
 
-namespace Pronko\HelloWorld\Controller\Index;
+namespace Devchannel\HelloWorld\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\HttpGetActionInterface;
-// use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action implements HttpGetActionInterface
 {
 	public function execute()
 	{
-		echo 'Hello world!!!';
-		// return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+		$page = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+		return $page;
 	}
 }
