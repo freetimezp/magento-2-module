5143652edb28171e72ea55da77018845721909a2
freetimezp <kraftukrnet@ukr.net>
2019-12-10 22:26:33 +0200

    Magento 2. Learning create block...

--- Whidegroup/Lastupdated/Block/Catalog/Product/View/Lastupdatedblock.php
+++ Whidegroup/Lastupdated/Block/Catalog/Product/View/Lastupdatedblock.php
@@ -1,16 +1,15 @@
 <?php
+//namespace Whidegroup\Lastupdated\Block\Catalog\Product\View;
  
-namespace Whidegroup\Lastupdated\Block\Catalog\Product\View;
+//use Magento\Catalog\Block\Product\Context;
+//use Magento\Catalog\Block\Product\AbstractProduct;
  
-use Magento\Catalog\Block\Product\Context;
-use Magento\Catalog\Block\Product\AbstractProduct;
- 
-class View extends AbstractProduct
-{
-    public function __construct(
-        Context $context,
-        array $data
-    ) {
-        parent::__construct($context, $data);
-    }
-}
+//class View extends AbstractProduct
+//{
+//    public function __construct(
+//        Context $context,
+//        array $data
+//    ) {
+//        parent::__construct($context, $data);
+//    }
+//}
---
+++ Whidegroup/Lastupdated/Block/Newblock.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Whidegroup\Lastupdated\Block;
+ 
+use Magento\Framework\View\Element\Template;
+ 
+class Newblock extends Template
+{
+	
+}
